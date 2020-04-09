c27e03cbf6e31e8121ea38a1c8a83c7d9c7f43ef
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-04-09 21:02:08 +0200

    Move animation into priceCard to avoid repetition

--- src/components/priceCard.js
+++ src/components/priceCard.js
@@ -1,13 +1,25 @@
 import React from "react"
 import styled from "styled-components"
+import { motion } from "framer-motion"
 
 const PriceCard = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>
+  return (
+    <Wrapper
+      initial={{ opacity: 0, y: 15 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.4,
+        duration: 0.4,
+      }}
+    >
+      {children}
+    </Wrapper>
+  )
 }
 
 export default PriceCard
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   padding: 20px;
   background-color: #111;
   border-radius: 10px;
