7dd13a53b994332da9e8f490f15a2b34078dd540
--global <frransier@gmail.com>
2020-04-10 13:08:44 +0200

    better bg

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import Image from "../images/insp/test.jpeg"
+import Image from "../images/insp/test.jpg"
 import { AiOutlineDownCircle } from "react-icons/ai"
 import RSSInput from "./rssInput"
 
